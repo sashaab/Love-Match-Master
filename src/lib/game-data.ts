9a309a3c97d29e77ad51c70944861a2ff26bdb5f
Sasha Abdullin <sashaabblog@gmail.com>
2025-08-01 10:35:46 +0000

    а где пары которые нельзя соединять?

--- src/lib/game-data.ts
+++ src/lib/game-data.ts
@@ -1,10 +1,11 @@
+
 import type { Celebrity } from './types';
 
 export const celebritiesData: Celebrity[] = [
   // Пары (Couples)
-  { id: '1', type: 'celebrity', name: 'Justin Bieber', imageUrl: 'https://placehold.co/150x150.png', partner: 'Hailey Bieber' },
+  { id: '1', type: 'celebrity', name: 'Justin Bieber', imageUrl: 'https://placehold.co/150x150.png', partner: 'Hailey Bieber', exes: ['Selena Gomez'] },
   { id: '2', type: 'celebrity', name: 'Hailey Bieber', imageUrl: 'https://placehold.co/150x150.png', partner: 'Justin Bieber' },
-  { id: '3', type: 'celebrity', name: 'Taylor Swift', imageUrl: 'https://placehold.co/150x150.png', partner: 'Travis Kelce' },
+  { id: '3', type: 'celebrity', name: 'Taylor Swift', imageUrl: 'https://placehold.co/150x150.png', partner: 'Travis Kelce', exes: ['Harry Styles', 'The Weeknd'] },
   { id: '4', type: 'celebrity', name: 'Travis Kelce', imageUrl: 'https://placehold.co/150x150.png', partner: 'Taylor Swift' },
   { id: '5', type: 'celebrity', name: 'Blake Lively', imageUrl: 'https://placehold.co/150x150.png', partner: 'Ryan Reynolds' },
   { id: '6', type: 'celebrity', name: 'Ryan Reynolds', imageUrl: 'https://placehold.co/150x150.png', partner: 'Blake Lively' },
@@ -13,16 +14,16 @@ export const celebritiesData: Celebrity[] = [
   { id: '9', type: 'celebrity', name: 'Tom Holland', imageUrl: 'https://placehold.co/150x150.png', partner: 'Zendaya' },
   { id: '10', type: 'celebrity', name: 'Zendaya', imageUrl: 'https://placehold.co/150x150.png', partner: 'Tom Holland' },
   
-  // Бывшие и одиночки (Exes & Fillers) - Список "exes" очищен
-  { id: '11', type: 'celebrity', name: 'Selena Gomez', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
+  // Бывшие и одиночки (Exes & Fillers)
+  { id: '11', type: 'celebrity', name: 'Selena Gomez', imageUrl: 'https://placehold.co/150x150.png', exes: ['Justin Bieber', 'The Weeknd'] },
   { id: '12', type: 'celebrity', name: 'Shawn Mendes', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
   { id: '13', type: 'celebrity', name: 'Drake', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
   { id: '14', type: 'celebrity', name: 'Kendall Jenner', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
   { id: '15', type: 'celebrity', name: 'Leonardo DiCaprio', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
   { id: '16', type: 'celebrity', name: 'Scarlett Johansson', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
-  { id: '17', type: 'celebrity', name: 'Harry Styles', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
+  { id: '17', type: 'celebrity', name: 'Harry Styles', imageUrl: 'https://placehold.co/150x150.png', exes: ['Taylor Swift'] },
   { id: '18', type: 'celebrity', name: 'Rihanna', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
-  { id: '19', type: 'celebrity', name: 'The Weeknd', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
+  { id: '19', type: 'celebrity', name: 'The Weeknd', imageUrl: 'https://placehold.co/150x150.png', exes: ['Selena Gomez'] },
   { id: '20', type: 'celebrity', name: 'Gigi Hadid', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
   { id: '21', type: 'celebrity', name: 'Dua Lipa', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
   { id: '22', type: 'celebrity', name: 'Chris Evans', imageUrl: 'https://placehold.co/150x150.png', exes: [] },
