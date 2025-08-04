47c4a4950e42279d305d67d77772f9fab7bc5e5a
Sasha Abdullin <sashaabblog@gmail.com>
2025-08-04 07:10:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button data-sideb

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -262,24 +262,21 @@ Sidebar.displayName = "Sidebar"
 const SidebarTrigger = React.forwardRef<
   React.ElementRef<typeof Button>,
   React.ComponentProps<typeof Button>
->(({ className, onClick, ...props }, ref) => {
+>(({ className, onClick, children, ...props }, ref) => {
   const { toggleSidebar } = useSidebar()
 
   return (
     <Button
       ref={ref}
       data-sidebar="trigger"
-      variant="ghost"
-      size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn(className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
       }}
       {...props}
     >
-      <PanelLeft />
-      <span className="sr-only">Toggle Sidebar</span>
+      {children || <PanelLeft />}
     </Button>
   )
 })
