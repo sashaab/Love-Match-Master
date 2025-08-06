bffeff847512b8e882323cdf1de2e3c0864ffa0d
Sasha Abdullin <sashaabblog@gmail.com>
2025-08-06 07:43:57 +0000

    и вставь еще один счетчик  <!-- Yandex.Metrika counter --> <script type=

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,19 @@ export default async function RootLayout({
             gtag('config', 'G-73Z7P5T108');
           `}
         </Script>
+        <Script id="yandex-metrika" type="text/javascript">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103613722', 'ym');
+
+            ym(103613722, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/103613722" style={{position:'absolute', left:'-9999px'}} alt="" /></div></noscript>
       </body>
     </html>
   );
